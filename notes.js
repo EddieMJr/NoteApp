ce31c2730d62e81409ae864fc08fab713a7dad6e
Eddie Millsaps Jr <164912904+EddieMJr@users.noreply.github.com>
2025-08-25 21:14:08 -0400

    completed section 2 & 3 of note app

--- notes.js
+++ notes.js
@@ -1,4 +1,80 @@
-const getNotes = function (){
+const fs = require ('fs')
+const chalk = require('chalk')
+
+const getNotes = () => {
     return 'Your notes...'
 }
-module.exports = getNotes
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('new note added!')
+    } else {
+        console.log('note title taken!')
+    }
+}
+
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter((note) => note.title !== title)
+
+    if (notes.length > notesToKeep.length) {
+        console.log('note removed!')
+        saveNotes(notesToKeep)
+    } else {
+        console.log('no note found!')
+    }
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+const listNotes = () => {
+    const notes = loadNotes()
+
+    console.log('your notes!')
+
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+    if (note) {
+        console.log(note.title)
+        console.log(note.body)
+    } else (
+        console.log('Note not found')
+    )
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+}
